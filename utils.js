bc4893325aff9573cb78ba159aa4dbb4902bf926
compzombie <mlmyerson@gmail.com>
2025-02-12 13:05:07 -0500

    moved utility functions

---
+++ utils.js
@@ -0,0 +1,43 @@
+// Utility Functions for ColorEconomy
+
+function randomColor() {
+  return `hsl(${Math.floor(Math.random() * 360)}, 80%, 60%)`;
+}
+
+function randRange(min, max) {
+  return min + Math.random() * (max - min);
+}
+
+function pointInPolygon(point, polygon) {
+  let inside = false;
+  for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+    const xi = polygon[i].x, yi = polygon[i].y;
+    const xj = polygon[j].x, yj = polygon[j].y;
+    const intersect = ((yi > point.y) !== (yj > point.y)) &&
+                      (point.x < (xj - xi) * (point.y - yi) / (yj - yi) + xi);
+    if (intersect) inside = !inside;
+  }
+  return inside;
+}
+
+function getSickColor(color, sickFactor) {
+  const m = color.match(/hsl\((\d+),\s*(\d+)%\s*,\s*(\d+)%\)/);
+  if (!m) return color;
+  let h = m[1], s = parseInt(m[2], 10), l = parseInt(m[3], 10);
+  let newS = Math.round(s * (1 - sickFactor));
+  return `hsl(${h}, ${newS}%, ${l}%)`;
+}
+
+function blendHSL(color1, color2, weight) {
+  const m1 = color1.match(/hsl\((\d+),\s*(\d+)%\s*,\s*(\d+)%\)/);
+  const m2 = color2.match(/hsl\((\d+),\s*(\d+)%\s*,\s*(\d+)%\)/);
+  if (!m1 || !m2) return color1;
+  let h1 = parseInt(m1[1]), s1 = parseInt(m1[2]), l1 = parseInt(m1[3]);
+  let h2 = parseInt(m2[1]), s2 = parseInt(m2[2]), l2 = parseInt(m2[3]);
+  let dh = h2 - h1;
+  if (Math.abs(dh) > 180) { dh = dh > 0 ? dh - 360 : dh + 360; }
+  let h = (h1 + weight * dh + 360) % 360;
+  let s = Math.round(s1 * weight + s2 * (1 - weight));
+  let l = Math.round(l1 * weight + l2 * (1 - weight));
+  return `hsl(${h}, ${s}%, ${l}%)`;
+}
